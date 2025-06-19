0016b524f3744fe554517b1c688731c1cc769016
Kaleb <Kalebreid15@gmail.com>
2025-06-19 16:58:54 -0400

    Some bonus excercises past line 16

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace LINQ
+﻿
+
+List<string> videoGameNameLength = new List<string>() { "Call of Duty", "Pokemon", "Grand Theft Auto", "Apex", "Fortnite" };
+videoGameNameLength.Add("Fifa");
+videoGameNameLength.Add("Tomb Raider");
+
+
+var orderedVideoGameNameLength = videoGameNameLength.OrderBy(name => name.Length);
+
+
+foreach (var videoGameName in orderedVideoGameNameLength)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+Console.WriteLine(videoGameName); 
 }
+
+//var orderedVideoGameNameLength = videoGameNameLength.Where(name => name.Length > 5)
+// .OrderBy(name => name.Length)
+// .ThenBy(name => name);
+
+//foreach (var videoGameName in orderedVideoGameNameLength)
+//{
+//Console.WriteLine(videoGameName);
+//}
+
+//var orderedVideoGameNameLength = videoGameNameLength.Select(name => name.Length)
+//.OrderBy(length => length);
+
+//foreach (var length in orderedVideoGameNameLength)
+//{
+//Console.WriteLine(length);
+//}
+
+//var orderedVideoGameNameLength = videoGameNameLength.Sum(name => name.Length);
+//Console.WriteLine(orderedVideoGameNameLength);
+
+//foreach (var videoGameName in videoGameNameLength)
+
+
+
